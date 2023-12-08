98e760f0639f455ec959003b59b02264e6a5e805
pavan <siriyalapavankumar@gmail.com>
2023-12-08 15:34:16 +0530

    added skelton and rendered firebase storage to ui

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -31,7 +31,7 @@ function TableWrapper(
     const files: FileType[] = docs.docs.map((doc) => ({
       id: doc.id,
       fileName: doc.data().fileName || doc.id,
-      timeStamp: new Date(doc.data().timestamp?.seconds * 1000) 
+      timeStamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
       
         fullName: doc.data().fileName,
         downloadUrl: doc.data().dowloadURL,
